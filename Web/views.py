ae061167b6d77cfd94a4ca1c3c8257a41e9cee80
dercio-sinione <derciosinione@gmail.com>
2021-06-26 20:54:34 +0100

    Config. Redis

--- Web/views.py
+++ Web/views.py
@@ -10,10 +10,12 @@ import time
 def friends(request):
   cache_data = Red.get('api')
   if cache_data:
+    print('Got from Redis')
     return JsonResponse(cache_data, safe=False)
 
 
   time.sleep(2)
   obj = list(Friends.objects.values())
+  print('Got from Postgres')
   cache_data = Red.set("api", obj)
   return JsonResponse(obj, safe=False)
