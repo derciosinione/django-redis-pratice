ad7270ca9124fa3b6e3aa50342cc83c89ca60788
dercio-sinione <derciosinione@gmail.com>
2021-06-27 18:33:13 +0100

    Redis Conf

--- Web/utils.py
+++ Web/utils.py
@@ -16,15 +16,3 @@ class Redis:
 
     cache_data = json.loads(cache_data)
     return cache_data
-
-
-class Redis2:
-  def set(cache_key, data):
-    rds.set(cache_key, data)
-    return True
-
-  def get(cache_key):
-    cache_data = rds.get(cache_key)
-    if not cache_data:
-      return None
-    return cache_data
--- Web/views.py
+++ Web/views.py
@@ -5,17 +5,15 @@ from .models import Friends
 from .utils import Redis
 
   # return render(request, 'Web/render.html', {"obj": obj} )
+  # return JsonResponse(obj, safe=False)
 
 def friends_redis(request):
   cache_data = Redis.get(Friends.__name__)
   if cache_data:
-    # return JsonResponse(cache_data, safe=False)
     return render(request, 'Web/render.html', {"obj": cache_data} )
-
 
 
 def friends(request):
   obj = list(Friends.objects.values())
   Redis.set(Friends.__name__, obj)
   return render(request, 'Web/render.html', {"obj": obj} )
-  # return JsonResponse(obj, safe=False)
