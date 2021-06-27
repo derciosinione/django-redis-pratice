b0b9ea8e6f72bbc63c4be6621b5266c828404626
dercio-sinione <derciosinione@gmail.com>
2021-06-27 16:50:56 +0100

    Signals

--- Core/urls.py
+++ Core/urls.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 import debug_toolbar
 
-from Web.views import friends
+from Web.views import friends, friends_redis
 
 
 urlpatterns = [
     path('', friends),
+    path('redis/', friends_redis),
     path('admin/', admin.site.urls),
     path('__debug__/', include(debug_toolbar.urls)),
 ]
--- Web/views.py
+++ Web/views.py
@@ -9,10 +9,12 @@ from .utils import Redis
 def friends_redis(request):
   cache_data = Redis.get(Friends.__name__)
   if cache_data:
+    print("Got from Postgres")
     return JsonResponse(cache_data, safe=False)
 
 
 def friends(request):
   obj = list(Friends.objects.values())
   Redis.set(Friends.__name__, obj)
+  print("Got from Postgres")
   return JsonResponse(obj, safe=False)
