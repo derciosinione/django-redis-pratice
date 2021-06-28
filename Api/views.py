da75b2b54c40af69577909e70718d48139a65c34
dercio-sinione <derciosinione@gmail.com>
2021-06-28 01:40:24 +0100

    Dict To Model

--- Api/views.py
+++ Api/views.py
@@ -8,6 +8,7 @@ from django.db.models import CharField, TextField
 from django.forms.models import model_to_dict
 from django.core.serializers.json import DjangoJSONEncoder
 import json
+import time
   # return render(request, 'Web/render.html', {"obj": obj} )
   # return JsonResponse(obj, safe=False)
 
@@ -22,9 +23,14 @@ def friends(request):
   obj = list(Friends.objects.select_related().values())
   # print(obj[0])
   Redis.set(Friends.__name__, obj)
-  friends = list()
-  for item in obj:
-    # friends.id = item['id']
-    owner = Users.objects.get(pk=item['owner_id'])
-    friends.append(Friends(id=item['id'], name=item['name'], age=item['age'], email=item['email'], owner=owner, creation_date=item['creation_date']))
-  return render(request, 'Api/render.html', {"obj": friends})
+  # friends = list()
+  # for item in obj:
+  #   owner = Users.objects.get(pk=item['owner_id'])
+  #   friends.append(Friends(id=item['id'], name=item['name'], age=item['age'], email=item['email'], owner=owner, creation_date=item['creation_date']))
+  
+  start_time = time.time()
+  obj = [Friends(id=item['id'], name=item['name'], age=item['age'], email=item['email'], owner=Users.objects.get(pk=item['owner_id']), creation_date=item['creation_date'])
+               for item in obj]
+  end_time = time.time()
+  print(f'it take {(end_time-start_time):.4f}s')
+  return render(request, 'Api/render.html', {"obj": obj})
